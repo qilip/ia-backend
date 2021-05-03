e345636c0c1aa026140e81d7de1e4df4d7a4916c
qilip <qilip@qilip.io>
2021-05-03 10:51:14 +0000

    Prisma init

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import KoaRouter from 'koa-router';
+import { PrismaClient } from '@Prisma/client';
 
 const api = new KoaRouter();
+const prisma = new PrismaClient();
 
 api.get('/', (ctx, next) => {
     ctx.status = 200;
